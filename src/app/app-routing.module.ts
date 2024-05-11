ddc9368f0ff0e83d52b82a4ba92443f42833da90
Mazen Mohamed <eldessoukymazen16@gmail.com>
2024-05-11 18:19:50 +0300

    home-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { SeatsComponent } from './components/seats/seats.component';
 import { MoviePageComponent } from './components/movie-page/movie-page.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'movies', component: HomeComponent },
   { path: 'movie', component: MoviePageComponent },
   { path: 'seats', component: SeatsComponent },
   { path: '**', pathMatch: 'full', component: PageNotFoundComponent },
