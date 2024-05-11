8312a0bd78474349bc8bd202079b2014201b50eb
Mahmoud1055 <mahmoudkamal105d@gmail.com>
2024-05-12 00:45:17 +0300

    adding the login page and user service and adding login route

---
+++ src/app/services/users.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  constructor(private http:HttpClient) { }
+  private DB_U_url="http://localhost:3000/users"
+  login=false
+  getAllUsers(){
+    return this.http.get(this.DB_U_url)
+  }
+  getUsersByid(id:number){
+    return this.http.get(this.DB_U_url+"/"+id)
+
+}
+}
