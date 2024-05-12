cc1d7d4e70e2f3fd95606b58c2379044c534bd42
agamy5280 <marawan.agamy2010@hotmail.com>
2024-05-12 11:58:34 +0300

    Updated Logic when routing to movie details

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,24 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { MovieService } from '../../services/movie.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent implements OnInit{
-  allMovies:any;
-  constructor(private movieService:MovieService){
-  }
-  ngOnInit(){
+export class HomeComponent implements OnInit {
+  allMovies: any;
+  constructor(private movieService: MovieService, private router: Router) {}
+  ngOnInit() {
     this.movieService.getAllMovies().subscribe({
-      next:(data)=>{
+      next: (data) => {
         this.allMovies = data;
       },
-      error:(err)=>{
+      error: (err) => {
         console.log(err);
-      }
+      },
     });
   }
-  
+  navigateToMoviePage(id: any): void {
+    this.router.navigate(['/movie'], {
+      queryParams: { id: id },
+    });
+  }
 }
