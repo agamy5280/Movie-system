347e4422dc7ad3cf6f6c1c4d05405c1f5ae2a2e6
Yosra299 <yosra.attaher@gmail.com>
2024-05-12 12:00:24 +0300

    changes

--- src/app/components/movie-page/movie-page.component.ts
+++ src/app/components/movie-page/movie-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-movie-page',
@@ -7,8 +8,10 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
   styleUrls: ['./movie-page.component.scss'] // Use styleUrls instead of styleUrl
 })
 export class MoviePageComponent {
-  constructor(private sanitizer: DomSanitizer) { }
-
+  constructor(private sanitizer: DomSanitizer, private route : ActivatedRoute) {
+    // const id = this.route.snapshot.params['id'];
+    // =>> url =>> movie/1
+   }
   getSafeUrl(url: string): SafeResourceUrl {
     return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   } 
