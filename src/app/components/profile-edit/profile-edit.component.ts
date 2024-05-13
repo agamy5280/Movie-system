6aaab5bffae373f75830a9669fde460a43868294
Yosra299 <yosra.attaher@gmail.com>
2024-05-13 13:06:15 +0300

    finish logic of edit profile page, adding some service to user service & updating the database

--- src/app/components/profile-edit/profile-edit.component.ts
+++ src/app/components/profile-edit/profile-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { UsersService } from '../../services/users.service';
 
 @Component({
   selector: 'app-profile-edit',
@@ -7,11 +9,13 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrl: './profile-edit.component.scss'
 })
 export class ProfileEditComponent {
+userID:any;
+Udata:any = [];
   myform= new FormGroup({
-    FirstName: new FormControl("",[Validators.required,Validators.minLength(3)]),
-    LastName: new FormControl("",[Validators.required,Validators.minLength(3)]),
-    age: new FormControl(0,[Validators.min(20),Validators.max(30),Validators.required]),
-    email: new FormControl("",[Validators.email])
+    FirstName: new FormControl("",[Validators.required,Validators.minLength(3), Validators.pattern('[A-Za-z]+')]),
+    LastName: new FormControl("",[Validators.required,Validators.minLength(3),  Validators.pattern('[A-Za-z]+')]),
+    phoneNumber: new FormControl(0,[Validators.required, Validators.min(1000000000), Validators.max(99999999999),Validators.pattern('[0-9]+')]),
+    email: new FormControl("",[Validators.email,Validators.required])
 
   })
   add(){
@@ -20,7 +24,34 @@ export class ProfileEditComponent {
     }
     else{
       alert("valid data")
+      // this.Udata.firstname=this.myform.value.FirstName;
+      // this.Udata.lastName=this.myform.value.LastName;
+      // this.Udata.phoneNumber=this.myform.value.phoneNumber;
+      // this.Udata.email=this.myform.value.email;
+      this.usersService.updateObjectById(this.userID,this.myform.value).subscribe();
+      
     }
   }
+
+constructor(private route : ActivatedRoute,
+  private usersService: UsersService,
+){
+
+}
+
+ngOnInit(): void {
+  this.route.queryParams.subscribe(async(params)=>{
+    this.userID= params['id'];
+    (await this.usersService.getUser(this.userID)).subscribe({
+      next:(data:any)=>{
+        this.Udata = data;
+        console.log(this.Udata);
+        
+
+      }
+    })
+  })
+}
+
 
 }
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -15,4 +16,24 @@ export class UsersService {
   getUsersByid(id:number){
     return this.http.get(this.DB_U_url+"/"+id)
   }
+
+  
+  // return whole user object based on ID
+  async getUser(id:any){
+    return this.http.get(this.DB_U_url).pipe(
+      map((data:any)=>{
+        return data.find((user:{id:any}) => user.id === id);
+      })
+    )
+  }
+
+  // send updated user to json file
+  updateObjectById(id: number, updatedObject: any): Observable<any> {
+    return this.http.put<any>(`${this.DB_U_url}/${id}`, updatedObject);
+  }
+
+
+
+
+
 }
