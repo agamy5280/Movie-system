af4effb00fb61932dc6e8ef2ef370e2d9157f817
Mahmoud1055 <mahmoudkamal105d@gmail.com>
2024-05-16 13:58:52 +0300

    .

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,25 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, map } from 'rxjs';
-import { User } from '../model/User';
+import { Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
   USERS:any 
-  private userSubject :BehaviorSubject<User|null>;
-  user:User|null = null;  
-  private DB_U_url="http://localhost:3000/users";
 
   constructor(private http:HttpClient) { 
-
-    if(localStorage.getItem('userData')){
-      this.user = JSON.parse(localStorage.getItem('userData') as string);
-    }
-    this.userSubject = new BehaviorSubject<User|null>(this.user);
-
-
     this.getAllUsers().subscribe({
       next:(data)=>{this.USERS=data;
       },
@@ -29,22 +18,8 @@ export class UsersService {
      }
    );
   }
-  // return observe user
-  getUserObservable(){
-    return this.userSubject.asObservable();
-  }
-
-  // clear local storage 
-  removeUserFromLocalStorage(){
-    localStorage.clear();
-    this.userSubject.next(null);
-  }
-  //set user 
-  setUser(user1:User){
-    localStorage.setItem('userData', JSON.stringify(user1));
-    this.userSubject.next(user1);
-  }
-
+  private DB_U_url="http://localhost:3000/users"
+  login=false;
   getAllUsers(){
     return this.http.get(this.DB_U_url)
   }
@@ -68,18 +43,23 @@ export class UsersService {
   }
  logIn(email:any,pass:any){
    
- for( const user of this.USERS){
+ for( let i = 0; i < this.USERS.length; i++){
+  const user = this.USERS[i];
   if (user.email==email){
     if(user.password==pass){
       return user
 
     }else{
-      return 1
+      
+      return 2
     }
 
-  }else{
-    return 2
+  }else  if (i === this.USERS.length - 1) {
+    return 1
   }
-}
-  
+
+ }
+
+  }
+
 }
