6a01ae51d9509c6a305886380be12bdbb5ac48e6
agamy5280 <marawan.agamy2010@hotmail.com>
2024-05-18 02:16:53 +0300

    Added price and movie Title to prev reservations

--- src/app/components/prev-reservations/reservation/reservation.component.ts
+++ src/app/components/prev-reservations/reservation/reservation.component.ts
@@ -4,22 +4,18 @@ import { MovieService } from '../../../services/movie.service';
 @Component({
   selector: 'app-reservation',
   templateUrl: './reservation.component.html',
-  styleUrl: './reservation.component.scss'
+  styleUrl: './reservation.component.scss',
 })
-export class ReservationComponent implements OnInit{
-  @Input() reservation:any;
-  movie:any;
-  constructor(private movieService:MovieService){
-    
-  }
-  
+export class ReservationComponent implements OnInit {
+  @Input() reservation: any;
+  movie: any;
+  constructor(private movieService: MovieService) {}
+
   ngOnInit(): void {
-    this.movieService.getShowTimes(this.reservation.id).subscribe(
-      {
-        next:(data)=>{
-          this.movie = data;
-        }
-      }
-    );
+    this.movieService.getShowTimes(this.reservation.id).subscribe({
+      next: (data) => {
+        this.movie = data;
+      },
+    });
   }
 }
